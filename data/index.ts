03532b4c2b77bda3b081b46e5801e44e2d38644a
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-05-19 02:29:45 +0530

    priyanshu-tariyal-portfolio

---
+++ data/index.ts
@@ -0,0 +1,212 @@
+export const navItems = [
+  { name: "Home", link: "#home" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Helping small businesses get established online",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName:
+      "absolute right-0 bottom-0 h-[28.5rem] md:w-96 w-60 rounded-2xl opacity-50",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b1.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    pinTitle: "Working...",
+    title: "T-store",
+    des: "Online store in this store admin can manage the products.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/sass.svg"],
+    link: "https://t-store-two.vercel.app/",
+  },
+  {
+    id: 2,
+    pinTitle: "Completed",
+    title: "Customize Outfit 3D",
+    des: "Interactive clothing customization website built with React.js & 3D rendering (Three.js).",
+    img: "/p2.png",
+    iconLists: ["/re.svg", "/tail.svg", "/three.svg", "ex.svg"],
+    link: "https://customize-outfits-3d.netlify.app/",
+  },
+  {
+    id: 3,
+    pinTitle: "Completed",
+    title: "My 3D Portfolio",
+    des: "Interactive 3D portfolio with React.js & Three.js, featuring user-friendly Contact and About sections, and a dynamic Projects page.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/three.svg"],
+    link: "https://priyanshu-tariyal-portfolio.netlify.app/",
+  },
+  {
+    id: 4,
+    pinTitle: "Completed",
+    title: "EpiGenerator",
+    des: "Built an AI image generator with user prompts, intuitive UI, and community sharing for collaborative creativity.",
+    img: "/p4.png",
+    iconLists: ["/re.svg", "/tail.svg", "ex.svg"],
+    link: "https://ai-image-generator-six-lovat.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Freelance Web Development",
+    desc: "Led the development of a web application for a client, from initial concept to deployment on a web host.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    slink: "https://github.com/priyanshutariyal02",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    slink: "https://x.com/PriyanshuTariy7?t=3zXD3YVVqMs3wiiZLrizeQ&s=09",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    slink: "https://www.linkedin.com/in/priyanshu-tariyal-92900021a",
+  },
+];
